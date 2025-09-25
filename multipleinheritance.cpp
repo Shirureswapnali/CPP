070640489914d45ebc39a068b196a3efa4cc07d7
SWAPNALI SHIRURE <156194578+Shirureswapnali@users.noreply.github.com>
2025-09-25 21:11:27 +0530

    Create multipleinheritance.cpp

---
+++ multipleinheritance.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+    public:
+    Student()
+    {
+        
+    }
+    void show()
+    {
+        cout<<"This is Parent classs"<<endl;
+    }
+};
+class Emp
+{
+    public:
+    Emp()
+    {
+        
+    }
+    void showdata()
+    {
+        cout<<"This is a Child class"<<endl;
+    }
+};
+class Manager:public Emp, public Student
+{
+    public:
+    Manager()
+    {
+        
+    }
+    void showda()
+    {
+        cout<<"This is a Manager class";
+    }
+};
+int main()
+{
+    Manager m1;
+    m1.show();
+    m1.showdata();
+    m1.showda();
+    return 0;
+}
